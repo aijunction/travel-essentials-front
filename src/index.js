83007ed48de0e1ffbc1cf136fe8fc7c4dd029e77
aijunction <officialaijunction@gmail.com>
2024-01-06 15:01:55 -0500

    dashboard changes

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
+import "assets/scss/argon-dashboard-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
 import Landing from "views/examples/Landing.js";
@@ -29,6 +30,8 @@ import Login from "views/examples/Login.js";
 import Profile from "views/examples/Profile.js";
 import Register from "views/examples/Register.js";
 import Products from "views/examples/Products.js";
+import AuthLayout from "layouts/Auth.js";
+import AdminLayout from "layouts/Admin.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -41,7 +44,48 @@ root.render(
       <Route path="/login-page" exact element={<Login />} />
       <Route path="/profile-page" exact element={<Profile />} />
       <Route path="/register-page" exact element={<Register />} />
+      <Route path="/admin/*" element={<AdminLayout />} />
+      <Route path="/auth/*" element={<AuthLayout />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
 );
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+// import "assets/plugins/nucleo/css/nucleo.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// // import "assets/scss/argon-dashboard-react.scss";
+
+// import AdminLayout from "layouts/Admin.js";
+// import AuthLayout from "layouts/Auth.js";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/admin/*" element={<AdminLayout />} />
+//       <Route path="/dashboard/*" element={<AuthLayout />} />
+//       <Route path="*" element={<Navigate to="/admin/index" replace />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
